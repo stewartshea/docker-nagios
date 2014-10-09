f4b696cb8a8c646a35d771d98ebdc7122aec7a5b
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-09 19:34:24 -0400

    Update postfix.sh

--- postfix.sh
+++ postfix.sh
@@ -3,4 +3,4 @@
 # `/sbin/setuser xxxxx` runs the given command as the user `xxxxx`.
 # If you omit that part, the command will be run as root.
 
-exec /usr/lib/postfix/master -d -c /etc/postfix
+exec /usr/lib/postfix/master -d -c /etc/postfix  >>/var/log/postfix.log 2>&1
