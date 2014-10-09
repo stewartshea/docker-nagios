6c98b419779167a2eb69744b82da468b7c750884
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-09 14:58:04 -0400

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+### In nagios.sh (make sure this file is chmod +x):
+# `/sbin/setuser xxxxx` runs the given command as the user `xxxxx`.
+# If you omit that part, the command will be run as root.
 
 exec /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg
