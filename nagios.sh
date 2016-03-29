ccca82c8848b0ebfe8e73f7380577d55e7638829
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-03-29 11:21:08 -0400

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### In nagios.sh (make sure this file is chmod +x):
-# `/sbin/setuser xxxxx` runs the given command as the user `xxxxx`.
+# `chpst -u root` runs the given command as the user `xxxxx`.
 # If you omit that part, the command will be run as root.
 
-exec /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg  >>/var/log/nagios.log 2>&1
+exec chpst -u root /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg 2>&1
