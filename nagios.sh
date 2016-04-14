0b7b8bf77d35e89101bd5e0e4e7b938c9422eefd
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-13 21:01:36 -0400

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -3,4 +3,6 @@
 # `chpst -u root` runs the given command as the user `xxxxx`.
 # If you omit that part, the command will be run as root.
 
+sv -w4 check apache2
+
 exec chpst -u root /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg 2>&1
